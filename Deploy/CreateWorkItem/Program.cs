384558608ba21f72a0c86bf0a3eb9b44884a5f87
Jessel <rebello.jessel@gmail.com>
2017-04-06 11:18:55 +0530

    Added fair sln

---
+++ Deploy/CreateWorkItem/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Xml.Schema;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace CreateWorkItem
+{
+    internal class Program
+    {
+        private static bool ValidateArguments(string[] args)
+        {
+            if (args.Length != 4)
+            {
+                return false;
+            }
+            int integer;
+            if (!Int32.TryParse(args[0], out integer))
+            {
+                return false;
+            }
+            if (!args[1].Contains("\\"))
+            {
+                return false;
+            }
+            if (!args[2].Contains("\\"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 1 - WorkItemId
+        /// 2 - AreaPath
+        /// 3 - IterationPath
+        /// 4 - Assigned To Name
+        /// example
+        ///     CreateWorkItem.exe   1234562  TIA\Development\TIA Portal\Engineering Platform\UIA\Library  "TIA\TIA Portal\V15\V15.0.0.0\Inc 01" "Bhaskar Babu, T. (CT DD DS AA DF-PD ES) (IN002.IC006714)"
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            if (!ValidateArguments(args))
+            {
+                Console.WriteLine(@"
+Syntax/Usage : CreateWorkItem.exe   1  2  3  4
+ 1 - WorkItemId (Request)
+ 2 - AreaPath
+ 3 - IterationPath
+ 4 - Assigned To Name
+example
+    CreateWorkItem.exe   123456   ""TIA\Development\TIA Portal\Engineering Platform\UIA\Library""   ""TIA\TIA Portal\V15\V15.0.0.0\Inc 01""   ""Bhaskar Babu, T. (CT DD DS AA DF-PD ES) (IN002.IC006714)""
+
+N O T E : Use double quotes when the argument has spaces...
+
+");
+                Environment.Exit(-1);
+            }
+            try
+            {
+                int idd = Convert.ToInt32(args[0]);
+                //ParseArguments();
+                TfsHandler.CreateWorkItem(idd,
+                    "https://iiaastfs.ww004.siemens.net:443/tfs/tia",
+                    args[3],
+                    "Feature",
+                    "RQ#",
+                    $"{idd} - ",//@"TIA\Development\TIA Portal\Engineering Platform\UIA\Library",
+                    args[1],//"Bhaskar Babu, T. (CT DD DS AA DF-PD ES) (IN002.IC006714)",
+                    args[2]);
+            }
+            catch (Exception e)
+            {
+                var x = e;
+                do
+                {
+                    Console.WriteLine($"Exception of type {x.GetType().Name} & Message='{x.Message}'");
+                    Console.WriteLine(x.StackTrace);
+                    x = x.InnerException;
+                } while (x != null);
+            }
+        }
+    }
+}
---
+++ Deploy/CreateWorkItem/TfsHandler.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CreateWorkItem
+{
+    public class TfsHandler
+    {
+
+        void Query(WorkItemStore workItemStore, string selectedProject, string folder, List<string> listQueries)
+        {
+            var project = workItemStore.Projects[selectedProject];
+            QueryHierarchy queryHierarchy = project.QueryHierarchy;
+            var queryFolder = queryHierarchy as QueryFolder;
+            QueryItem queryItem = queryFolder[folder];
+            queryFolder = queryItem as QueryFolder;
+            foreach (var item in queryFolder)
+            {
+                listQueries.Add(item.Name);
+            }
+        }
+
+        internal static void CreateWorkItem(int srcWorkItemId, string tfsuri, string assignedtoname, string wiType,
+            string titleprefix, string externalidsuffix, string area, string iterationPath)
+        {
+            //var tfsCreds = new TfsClientCredentials(new WindowsCredential(), false);
+            using (TfsTeamProjectCollection tfs = new TfsTeamProjectCollection(new Uri(tfsuri)))
+            {
+                ;
+                //TfsTeamProjectCollection targettfs = new TfsTeamProjectCollection(new Uri(tfsuri), tfsCreds);
+                try
+                {
+                    tfs.Authenticate();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.InnerException);
+                    return;
+                }
+
+                WorkItemStore wis = tfs.GetService<WorkItemStore>();
+                Project teamProject = wis.Projects["TIA"];
+                WorkItemType workItemType = teamProject.WorkItemTypes[wiType];
+
+                foreach (WorkItemType temp in teamProject.WorkItemTypes)
+                {
+                    if (temp.Name.ToLower().Equals(wiType.ToLower()))
+                    {
+                        wiType = temp.Name;
+                    }
+                }
+
+                WorkItem srcWorkItemwi = wis.GetWorkItem(srcWorkItemId);
+                string srcTitle = srcWorkItemwi.Title;
+                string srcType = srcWorkItemwi.Type.Name;
+
+                if (srcType != "Request")
+                {
+                    Console.WriteLine("CreateChildWI works only for Requests!");
+                    return;
+                }
+                WorkItemLinkTypeEnd wiTypeEnd;
+                wis.WorkItemLinkTypes.LinkTypeEnds.TryGetByName("Duplicates", out wiTypeEnd);
+
+                WorkItem newWorkItem = new WorkItem(workItemType);
+                newWorkItem.Title = $"{titleprefix}{externalidsuffix}{srcTitle}";
+                newWorkItem.AreaPath = area;
+                newWorkItem.Fields["Assigned To"].Value = assignedtoname;
+                newWorkItem.Fields["Feature Type"].Value = "Internal";
+                newWorkItem.IterationPath = iterationPath;
+                var wiLink = new WorkItemLink(wiTypeEnd, srcWorkItemId);
+                newWorkItem.WorkItemLinks.Add(wiLink);
+                bool linkedfeaturesalready = false;
+                foreach (WorkItemLink link in srcWorkItemwi.WorkItemLinks)
+                {
+                    int target = link.TargetId;
+                    WorkItem targetWorkItem = wis.GetWorkItem(target);
+                    if (targetWorkItem.Type.Name == wiType)
+                    {
+                        Console.WriteLine($"The Request {srcWorkItemId} has already linked Feature = {target}");
+                        linkedfeaturesalready = true;
+                    }
+                }
+                if (linkedfeaturesalready)
+                {
+                    Console.WriteLine($"Error: Found linked Features!!! ");
+                    return;
+                }
+                var issues = newWorkItem.Validate();
+                foreach (var issue in issues)
+                {
+
+                }
+                Console.WriteLine($"Error: Found {issues.Count} issues during validation of Feature Workitem!");
+                newWorkItem.Save();
+                Console.WriteLine($"Created New Feature with id: {newWorkItem.Id}");
+
+            }
+        }
+    }
+}
