63d9701c39781ac5207c0c3bf5c03b1ce1c67d52
Jessel <rebello.jessel@gmail.com>
2017-07-26 15:00:38 +0530

    Added pollasium

--- Deploy/CreateWorkItem/RunTool.cmd
+++ Deploy/CreateWorkItem/RunTool.cmd
@@ -5,32 +5,33 @@ call :RunTool > RunTool.log
 goto :eof
 
 :RunTool
-
-CreateWorkItem.exe   1083186   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Krishnamurthy, Chinmaya (CT DD DS AA DF-PD FH ES)"	
-CreateWorkItem.exe   1304694   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   1712261   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   1814496   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mondal, Suvradip (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   1822890   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Foertsch, Angelika (DF FA TIP DH FTH1)				
-CreateWorkItem.exe   1881194   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   1769151   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   2015970   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   2022874   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   2074818   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   2079058   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mishra, Anurag (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   2095372   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Rebello, Jessel (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   2116698   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Rebello, Jessel (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   2122153   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Kajevic, Samir (DF FA TIP DH FTH1)					
-CreateWorkItem.exe   2132753   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mondal, Suvradip (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   2193907   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Chaudhary, Ashutosh Kumar (CT DD DS AA DF-PD FH AT)	
-CreateWorkItem.exe   2203054   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Foertsch, Angelika (DF FA TIP DH FTH1)				
-CreateWorkItem.exe   2206602   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   2229661   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mondal, Suvradip (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   2240902   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
 CreateWorkItem.exe   2331953   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
-CreateWorkItem.exe   2346203   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Rebello, Jessel (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   2367459   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mondal, Suvradip (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   2371422   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Rebello, Jessel (CT DD DS AA DF-PD FH ES)"			
-CreateWorkItem.exe   2393454   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Amol Malang, Pankajkumar (CT DD DS AA DF-PD FH ES)"	
-CreateWorkItem.exe   2395481   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Kajevic, Samir (DF FA TIP DH FTH1)					
+CreateWorkItem.exe   2015970   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   1083186   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Krishnamurthy, Chinmaya (CT DD DS AA DF-PD FH ES)"	
+rem CreateWorkItem.exe   1304694   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   1712261   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   1814496   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mondal, Suvradip (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   1822890   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Foertsch, Angelika (DF FA TIP DH FTH1)				
+rem CreateWorkItem.exe   1881194   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   1769151   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   2015970   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   2022874   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   2074818   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   2079058   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mishra, Anurag (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   2095372   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Rebello, Jessel (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   2116698   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Rebello, Jessel (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   2122153   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Kajevic, Samir (DF FA TIP DH FTH1)					
+rem CreateWorkItem.exe   2132753   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mondal, Suvradip (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   2193907   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Chaudhary, Ashutosh Kumar (CT DD DS AA DF-PD FH AT)	
+rem CreateWorkItem.exe   2203054   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Foertsch, Angelika (DF FA TIP DH FTH1)				
+rem CreateWorkItem.exe   2206602   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   2229661   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mondal, Suvradip (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   2240902   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   2331953   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "M, Juliya James (CT DD DS AA DF-PD FH ES)"
+rem CreateWorkItem.exe   2346203   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Rebello, Jessel (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   2367459   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Mondal, Suvradip (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   2371422   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Rebello, Jessel (CT DD DS AA DF-PD FH ES)"			
+rem CreateWorkItem.exe   2393454   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Amol Malang, Pankajkumar (CT DD DS AA DF-PD FH ES)"	
+rem CreateWorkItem.exe   2395481   "TIA\Development\TIA Portal\Engineering Platform\UIA\Applications"  "TIA\TIA Portal\V15\V15.0.0.0"   "Kajevic, Samir (DF FA TIP DH FTH1)					
 
 goto :eof
